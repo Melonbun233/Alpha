7819ec88e843f8ed20597ea97617a25c88afc039
Yakoto <xht8723@vip.qq.com>
2020-05-03 21:28:38 -0700

    随便写点框架

---
+++ Assets/scripts/enemy_controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_controller : MonoBehaviour
+{
+    public float speed;//the moving speed parameter of enemy
+    public int hp;//enemy health
+
+    public Transform Base;//Link to base transformation
+
+    void Start()
+    {
+        //initialize enemy status
+        speed = 1f;
+        hp = 3;
+
+        //AI寻路
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = Base.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
