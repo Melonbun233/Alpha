4d96a6c982660e300c2750f5e43dda748d0acd80
Yakoto <xht8723@vip.qq.com>
2020-05-06 12:20:54 -0700

    worked half of the map generator.

--- Assets/Scripts/MapGen.cs
+++ Assets/Scripts/MapGen.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MapGen : MonoBehaviour
 {
-
+    //public parameters for UI
     public int rows;
     public int columns;
     public GameObject[] Base;
@@ -12,45 +12,110 @@ public class MapGen : MonoBehaviour
     public GameObject[] sideTiles;
     public GameObject[] enemySpawn;
 
+    //An empty to hold all maptiles
     private Transform mapHolder;
+
     private List<Vector3> gridPositions = new List<Vector3>();
+
+    //Mark base location for setting up map reasonablly.
+    private Vector3 _baseloc;
+    private int baselocMark;
+
 
     void InitialiseList()
     {
         gridPositions.Clear();
+        int row = rows * 10;
+        int colum = columns * 10;
 
-        for (int c = 0; c < columns; c++)
+        for (int c = 0; c < colum; c=c+10)
         {
-            for (int r = 0; r < rows; r++)
+            for (int r = 0; r < row; r=r+10)
             {
-                gridPositions.Add(new Vector3(c, r, 0f));
+                gridPositions.Add(new Vector3(c, 0f, r));
             }
         }
     }
 
+    //convert unhuman quaternion to euler rotation.
+    Quaternion ranRotation() {
+        Quaternion temp = new Quaternion(0, 0, 0, 0);
+        int temp2 = Random.Range(0, 4);
+        temp = Quaternion.Euler(0, 90 * temp2, 0);
+        return temp;
+    }
+
+    //Mark base location for setting up map reasonablly.
+    int markBase() {
+        int tempC = Random.Range(0, columns);
+        int tempR = Random.Range(0, rows);
+        _baseloc = new Vector3(tempC * 10, 0f, tempR * 10);
+        return tempC * tempR;
+    }
+
+    //set up base tiles.
+    void baseSetup() {
+        
+        int row = rows * 10;
+        int colum = columns * 10;
+        baselocMark = markBase();
+
+        for (int c = 0; c < colum; c = c + 10)
+        {
+            for (int r = 0; r < row; r = r + 10)
+            {
+                if (new Vector3(c,0f,r).Equals(_baseloc))
+                {
+                    GameObject toInstantiate = Base[Random.Range(0, Base.Length)];
+                    GameObject instance = Instantiate(toInstantiate, new Vector3(c, 0f, r), ranRotation()) as GameObject;
+                    break;
+                }
+            }
+        }
+    }
+
+    //set up enemy spawn tiles.
+    void spawnSetup() { 
+    
+    
+    
+    
+    
+    }
+
+
+    //fill out the rest of the map tiles.
     void mapSetup()
     {
         mapHolder = new GameObject("Map").transform;
+        int row = rows * 10;
+        int colum = columns * 10;
 
-        for (int c = 0; c < columns; c++)
+        for (int c = 0; c < colum; c = c+10)
         {
-            for (int r = 0; r < columns; r++)
+            for (int r = 0; r < row; r = r+10)
             {
-                GameObject toInstantiate = midTiles[Random.Range(0, midTiles.Length)];
-
-                GameObject instance = Instantiate(toInstantiate, new Vector3(c, r, 0f), Quaternion.identity) as GameObject;
-
-                instance.transform.SetParent(mapHolder);
+                Vector3 loc = new Vector3(c, 0f, r);
+                if (!loc.Equals(_baseloc))
+                {
+                    GameObject toInstantiate = midTiles[Random.Range(0, midTiles.Length)];
+                    GameObject instance = Instantiate(toInstantiate, new Vector3(c, 0f, r), ranRotation()) as GameObject;
+                    instance.transform.SetParent(mapHolder);
+                }
             }
         }
     }
-    // Start is called before the first frame update
+
     void Start()
     {
+
+        
+        
+        baseSetup();
         mapSetup();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
