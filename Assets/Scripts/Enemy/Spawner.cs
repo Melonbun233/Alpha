95d7087a3f5beabd6d1ae24aeb9c6b7d63019de6
Zhuohang Zeng <zeng_zh@foxmail.com>
2020-05-11 02:39:32 -0700

    moved CameraController and Destroyable to General folder

--- Assets/Scripts/Enemy/Spawner.cs
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -35,11 +35,6 @@ public class Spawner : Enemy
         _spawnCoolDown -= Time.deltaTime;
     }
 
-    // Spawner doesnt move nor attack
-    public override void attack(){}
-    public override void updateAttackTarget(){}
-    public override void move(){}
-    public override void updateMoveTarget(){}
 
     private void Spawn(GameObject enemy, Vector3 position, Quaternion rotation) {
         Instantiate(meleeEnemy, position, rotation);
