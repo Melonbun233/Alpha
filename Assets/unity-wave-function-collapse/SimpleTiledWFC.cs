e6b8b11247c78eac6a6cd32953d77b8bf65a7211
Zhuohang Zeng <zeng_zh@foxmail.com>
2020-05-10 22:23:33 -0700

    removed app after exiting play mode. Note: dont leave instantiated map in the editor mode as this would introduce git conflict quite easily

--- Assets/unity-wave-function-collapse/SimpleTiledWFC.cs
+++ Assets/unity-wave-function-collapse/SimpleTiledWFC.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using UnityEditor;
 #endif
 
-[ExecuteInEditMode]
+//[ExecuteInEditMode]
 public class SimpleTiledWFC : MonoBehaviour{
 	
 	public TextAsset xml = null;
