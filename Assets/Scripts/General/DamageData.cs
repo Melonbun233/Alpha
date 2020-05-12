8cbe9cb7e9849d70d1ed49e8238adb9baf6dbb21
Zhuohang Zeng <zeng_zh@foxmail.com>
2020-05-11 22:15:51 -0700

    added DamageData class for incomming damage. Need to modify destroyable in the future

---
+++ Assets/Scripts/General/DamageData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DamageType {
+    Physic,
+    Magic // probably will change later
+}
+// Damage data represents the type, amount of damage dealt to another destroyable
+public class DamageData
+{
+    public int amount;
+    public DamageType type;
+
+    public GameObject dealer;
+
+    DamageData(int _amount, DamageType _type, GameObject _dealer) {
+        amount = _amount;
+        type = _type;
+        dealer = _dealer;
+    }
+}
