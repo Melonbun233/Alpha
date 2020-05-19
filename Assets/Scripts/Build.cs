aa0d897079e2b17b80fdb3a223e70db9c89c7a7a
Yakoto <xht8723@vip.qq.com>
2020-05-19 14:53:13 -0700

    finished placing

---
+++ Assets/Scripts/Build.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Build : MonoBehaviour
+{
+
+    GameObject Tower;
+    GameObject toPlaceTower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnMouseUp()
+    {
+        if (Tower != null) { 
+            if(placement.toPlace.hit.transform.tag == "walls")
+            {
+                toPlaceTower = Instantiate(Tower, transform.position + placement.toPlace.wallOffset , transform.rotation) as GameObject;
+            }
+            else
+            {
+                toPlaceTower = Instantiate(Tower, transform.position + placement.toPlace.valleyOffset, transform.rotation) as GameObject;
+            }
+        }
+        
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            print(placement.toPlace.hit.transform.position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (placement.toPlace.tower != null)
+        {
+            Tower = placement.toPlace.tower;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/placement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placement : MonoBehaviour
+{
+    public static placement toPlace;
+
+    public GameObject towerModel;
+    public Vector3 cursor;
+    public GameObject tower;
+    public Vector3 wallOffset;
+    public Vector3 valleyOffset;
+
+    Ray ray;
+    public RaycastHit hit;
+
+    void Awake()
+    {
+        toPlace = this;
+    }
+
+    void OnMouseUp()
+    {
+        towerModel = GetComponent<TowerOption>().TowerModel;
+        tower = Instantiate(towerModel) as GameObject;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, 1000) && tower != null && hit.collider != GetComponent<Collider>())
+        {
+            if(hit.transform.tag == "walls")
+            {
+                tower.transform.position = hit.transform.position + wallOffset;
+            }
+            else
+            {
+                tower.transform.position = hit.transform.position + valleyOffset;
+            }
+            
+        }
+    }
+}
