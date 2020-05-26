6e591580ad6db8b80552bb0889e4f910d0cfd730
Zhuohang Zeng <zeng_zh@foxmail.com>
2020-05-25 21:59:02 -0700

    tested one fire ball projectile

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -9,16 +9,31 @@ public class Projectile : MonoBehaviour
     public GameObject attacker;
     public float speed;
 
+    public GameObject muzzlePrefab;
+    public GameObject hitPrefab;
+
+    public AudioClip shotSFX;
+    public AudioClip hitSFX;
+    public List<GameObject> trails;
+
+    private bool collided;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();    
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void FixedUpdate() {
         if (target == null) {
+            Destroy(gameObject);
             return;
         }
 
@@ -27,22 +42,30 @@ public class Projectile : MonoBehaviour
 
         transform.position = Vector3.MoveTowards(transform.position, 
             targetTransform.position, step);
-        transform.LookAt(targetTransform.position);
+        
+        Quaternion rotation = Quaternion.LookRotation(targetTransform.position);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, step);
     }
 
-    void OnCollisionEnter(Collision other) {
-        string tag = other.collider.tag;
-        if (tag == "valley") {
-            kill();
+    void OnTriggerEnter(Collider collider) {
+        if (collided) {
             return;
         }
 
-        if (tag != "Enemy") {
+        string tag = collider.tag;
+
+        // if (tag == "walls" || tag == "valley") {
+        //     OnHitVFX(collision);
+        //     return;
+        // }
+
+        if (tag != "Enemy") {    
             return;
         }
+
 
         // deal damage to the enemy
-        GameObject target = other.collider.gameObject;
+        GameObject target = collider.gameObject;
         Unit attackUnit = attacker.GetComponent<Unit>();
         DamageData damage = attackUnit.attackData.attackDamage;
         target.GetComponent<Destroyable>().receiveDamage(damage, attacker);
@@ -50,17 +73,76 @@ public class Projectile : MonoBehaviour
         // apply on attack event
         attackUnit.OnAttack(target);
 
-        kill();
+        // Play on hit vfx
+        OnHitVFX(collider);
     }
 
-    public void kill() {
-        Destroy(gameObject);
+    private void OnHitVFX(Collider collider) {
+        collided = true;
+
+        // Play on hit sfx
+        AudioSource audioSource = GetComponent<AudioSource>();
+        if (hitSFX != null && audioSource != null) {
+            audioSource.PlayOneShot(hitSFX);
+        }
+
+        // Destroy trails
+        foreach (GameObject trail in trails) {
+            ParticleSystem ps = trail.GetComponent<ParticleSystem>();
+            if (ps != null) {
+                ps.Stop();
+                Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+            }
+        }
+
+        speed = 0;
+
+        // On hit vfx
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, 
+            transform.position - collider.transform.position);
+        Vector3 position = transform.position;
+
+        if (hitPrefab != null) {
+            GameObject hitVFX = Instantiate(hitPrefab, position, rotation);
+            ParticleSystem ps = hitVFX.GetComponent<ParticleSystem>();
+
+            if (ps == null) {
+                // particle systems are in the child
+                var psChild = hitVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(hitVFX, psChild.main.duration);
+            } else {
+                Destroy(hitVFX, ps.main.duration);
+            }
+        }
+
+        StartCoroutine(destroyParticle(0f));
     }
 
-    public static GameObject spawn(GameObject prefab, Vector3 position, Quaternion rotation, 
+    private IEnumerator destroyParticle(float waitTime) {
+        if (transform.childCount > 0 && waitTime != 0) {
+			List<Transform> tList = new List<Transform> ();
+
+			foreach (Transform t in transform.GetChild(0).transform) {
+				tList.Add (t);
+			}		
+
+			while (transform.GetChild(0).localScale.x > 0) {
+				yield return new WaitForSeconds (0.01f);
+				transform.GetChild(0).localScale -= new Vector3 (0.1f, 0.1f, 0.1f);
+				for (int i = 0; i < tList.Count; i++) {
+					tList[i].localScale -= new Vector3 (0.1f, 0.1f, 0.1f);
+				}
+			}
+		}
+		
+		yield return new WaitForSeconds (waitTime);
+		Destroy (gameObject);
+    }
+
+    public static GameObject spawn(GameObject vfx, Vector3 position, Quaternion rotation, 
         GameObject attacker, GameObject target, float speed) {
 
-            if (prefab.GetComponent<Projectile>() == null) {
+            if (vfx.GetComponent<Projectile>() == null) {
                 Debug.Log("Cannot spawn non-projectile object");
                 return null;
             }
@@ -68,10 +150,8 @@ public class Projectile : MonoBehaviour
             GameObject projectiles = GameObject.Find("Projectiles");
             if (projectiles == null) {
                 projectiles = new GameObject("Projectiles");
-                // projectiles.transform.position = new Vector3(0, 0, 0);
-                // projectiles.transform.rotation = new Quaternion(0, 0, 0, 0);
             }
-            GameObject obj = Instantiate(prefab, position, rotation, projectiles.transform);
+            GameObject obj = Instantiate(vfx, position, rotation, projectiles.transform);
             obj.transform.LookAt(target.transform.position);
 
             Projectile projectile = obj.GetComponent<Projectile>();
