ac94f11ea1a66c051f7993e822a631c1b2a9b4a7
Zhuohang Zeng <zeng_zh@foxmail.com>
2020-06-02 06:13:07 -0700

    reduced a lot particle effects for performance

--- Assets/Scripts/Projectiles/ProjectileSetThunder.cs
+++ Assets/Scripts/Projectiles/ProjectileSetThunder.cs
@@ -22,12 +22,71 @@ public class ProjectileSetThunder : ProjectileSet
         // "Projectiles",
     };
 
+    public static readonly string[] modifyNamesLevel2 = {
+        "Sparks",
+        "Beam",
+        //"BeamWorld",
+    };
+
+    public static readonly string[] modifyNamesLevel3 = {
+        "Sparks",
+        "Beam",
+        //"BeamWorld",
+    };
+
+    public static readonly string[] modifyNamesLevel4 = {
+        "MainBeam",
+        // "GlowBeam",
+        // "Smoke",
+        "Particles",
+        // "Projectiles",
+    };
+
+    
+
     protected override void Start() {
         base.Start();
 
         addLevel(new LevelConfig(defaultPaths[0], modifyNamesLevel1, defaultModifyAction));
-        // addLevel(new LevelConfig(defaultPaths[1], defaultModifyNames, defaultModifyAction));
+        addLevel(new LevelConfig(defaultPaths[1], modifyNamesLevel2, defaultModifyAction));
         // addLevel(new LevelConfig(defaultPaths[2], defaultModifyNames, defaultModifyAction));
         // addLevel(new LevelConfig(defaultPaths[3], defaultModifyNames, defaultModifyAction));
     }
+
+    void waterDefaultModifyAction (GameObject obj, int mainTypeLevel, 
+        int subTypeLevel, Color color) {
+            if (color == CustomColors.noviceMainColor) {
+                return;
+            }
+            Color transparentColor = new Color(color.r, color.g, color.b, 60);
+            ParticleSystem ps = obj.GetComponent<ParticleSystem>();
+            if (ps != null) {
+                ParticleSystem.MainModule main = ps.main;
+                ParticleSystemRenderer renderer = obj.GetComponent<ParticleSystemRenderer>();
+
+                renderer.material.SetFloat("_Emission", 0.15f);
+
+                if (obj.name == "Beam") {
+                    renderer.material.SetColor("_Color", transparentColor * 0.03f);
+                    main.startColor = color;
+                } else {
+                    renderer.material.SetColor("_Color", color * 0.02f);
+                    main.startColor = color;
+                }
+
+                if (obj.name == "Particles") {
+                    renderer.material.SetFloat("_Emission", 0.7f);
+                    renderer.material.SetColor("_Color", color * 0.04f);
+                }
+                
+            }
+
+            // Trail
+            TrailRenderer tr = obj.GetComponent<TrailRenderer>();
+            if (tr != null) {
+                tr.startColor = color;
+                tr.material.SetFloat("_Emission", 0.1f);
+                tr.material.SetColor("_Color", color * 0.02f);
+            }
+        }
 }
